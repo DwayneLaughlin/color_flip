38fe886fd7fa024c1b2f410ce92555cd4371f11a
DwayneL <dr2@ameritech.net>
2020-07-01 18:37:10 -0500

    updated event listener to pick random color on initial click

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const colors = ["red", "green", "blue"];
 const click = document.getElementById("btn");
+const body = document.getElementById("body");
+const randomColor = colors[Math.floor(Math.random()*colors.length)]
 
 click.addEventListener("click", function(){
-    console.log("hey")
+    console.log(randomColor)
+    body.style.backgroundColor=randomColor
 })
