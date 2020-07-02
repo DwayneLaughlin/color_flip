482a6cc630693a88452f76bcff79596dec020f84
DwayneL <dr2@ameritech.net>
2020-07-01 21:50:13 -0500

    updates to click listner

--- app.js
+++ app.js
@@ -2,11 +2,9 @@ const colors = ["red", "green", "blue"];
 const click = document.getElementById("btn");
 const body = document.getElementById("back");
 
-
-const newColor = function(){
-    const randomColor = colors[Math.floor(Math.random()*colors.length)];
-    console.log(randomColor);
-body.style.backgroundColor=randomColor
-}
- click.addEventListener("click", newColor)
-
+const newColor = function () {
+  const randomColor = colors[Math.floor(Math.random() * colors.length)];
+  console.log(randomColor);
+  body.style.backgroundColor = randomColor;
+};
+click.addEventListener("click", newColor);
